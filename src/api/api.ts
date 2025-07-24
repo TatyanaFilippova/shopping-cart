4ee4a8e042c3ca287df4563afb4c0a6e7ac2396c
Tatyana <filippovat42@gmail.com>
2025-07-24 11:28:16 +0300

    api delete

--- src/api/api.ts
+++ src/api/api.ts
@@ -17,3 +17,33 @@ export async function deleteProduct(id: string) {
     console.error(error);
   }
 }
+
+export async function postProduct() {
+  try {
+    const response = await axios.post("http://localhost:3001/notes", {
+      notes: [
+        {
+          id: "1",
+          name: "Apple MacBook Air 13",
+          price: "110 000",
+          imgProduct: "/img/products/macbook.jpg",
+        },
+        {
+          id: "2",
+          name: "Apple watch",
+          price: "29 000",
+          imgProduct: "/img/products/apple-watch.jpg",
+        },
+        {
+          id: "3",
+          name: "Mac Pro",
+          price: "190 000",
+          imgProduct: "/img/products/mac-pro.jpg",
+        },
+      ],
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+  }
+}
--- src/components/CartFooter/index.tsx
+++ src/components/CartFooter/index.tsx
@@ -3,6 +3,7 @@ import "./style.scss";
 const CartFooter = () => {
   return (
     <footer className="cart-footer">
+      <button className="button">Сбросить</button>
       <div className="cart-footer__count">3 единицы</div>
       <div className="cart-footer__price">329 000 руб.</div>
     </footer>
